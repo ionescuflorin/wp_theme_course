1da0ce72e7076e71bdfa7495124da34b2c9d1d7d
Florin Ionescu <sii76354@adobe.com>
2020-03-02 13:04:27 +0200

    Disable caching only for dev mode

--- wp-content/themes/univ/functions.php
+++ wp-content/themes/univ/functions.php
@@ -1,17 +1,18 @@
 <!-- This is our behind the scene file -->
-<?php 
+<?php
 
-function university_files() {
+function university_files()
+{
     // here we can load or css or js
 
     // load js file
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
     // load custom font
     wp_enqueue_style('custom-google-font', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     // load font awesome
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    // laod css
-    wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+    // laod main css
+    wp_enqueue_style('university_main_styles', get_stylesheet_uri(), NULL, microtime());
 }
 // S4 V11 M15
 add_action('wp_enqueue_scripts', 'university_files');
