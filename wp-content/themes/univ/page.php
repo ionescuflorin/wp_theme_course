5d5f7dfc4abeef2a305df2a68b16ea29b866dab9
Florin Ionescu <sii76354@adobe.com>
2020-03-02 13:55:26 +0200

    Paren to children page breadcrumb

--- wp-content/themes/univ/page.php
+++ wp-content/themes/univ/page.php
@@ -3,23 +3,34 @@
 get_header();
 while (have_posts()) {
     the_post(); ?>
-     <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
-    <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"><?php the_title() ?></h1>
-      <div class="page-banner__intro">
-        <p>DONT FORGET TO REPLACE ME LATER</p>
-      </div>
-    </div>  
-  </div>
-
-  <div class="container container--narrow page-section">
-
-    <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span></p>
+    <div class="page-banner">
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+        <div class="page-banner__content container container--narrow">
+            <h1 class="page-banner__title"><?php the_title() ?></h1>
+            <div class="page-banner__intro">
+                <p>DONT FORGET TO REPLACE ME LATER</p>
+            </div>
+        </div>
     </div>
-    
-    <!-- <div class="page-links">
+
+    <div class="container container--narrow page-section">
+
+
+        <?php
+        //echo get_the_ID() // gives us the id if the current page that we are visiting
+        // echo wp_get_post_parent_id(get_the_ID());
+        $theParent = wp_get_post_parent_id(get_the_ID());
+        if ($theParent) { ?>
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title() ?></span></p>
+            </div>
+
+        <?php }
+        ?>
+
+
+
+        <!-- <div class="page-links">
       <h2 class="page-links__title"><a href="#">About Us</a></h2>
       <ul class="min-list">
         <li class="current_page_item"><a href="#">Our History</a></li>
@@ -27,11 +38,11 @@ while (have_posts()) {
       </ul>
     </div> -->
 
-    <div class="generic-content">
-      <?php the_content() ?>
-    </div>
+        <div class="generic-content">
+            <?php the_content() ?>
+        </div>
 
-  </div>
+    </div>
 
 <?php }
 
